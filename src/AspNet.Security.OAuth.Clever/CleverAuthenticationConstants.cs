b7a5f1ce5472cbf1293979eaa8666e506a40dd46
David Cramer <david.cramer@strongmind.com>
2021-06-15 10:23:46 -0700

    adding Clever auth

---
+++ src/AspNet.Security.OAuth.Clever/CleverAuthenticationConstants.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+namespace AspNet.Security.OAuth.Clever
+{
+    /// <summary>
+    /// Contains constants specific to the <see cref="CleverAuthenticationHandler"/>.
+    /// </summary>
+    public static class CleverAuthenticationConstants
+    {
+        public static class Claims
+        {
+            public const string Avatar = "urn:clever:avatar";
+            public const string Avatar150 = "urn:clever:avatar150";
+        }
+    }
+}
